c31ff9268bc93698385582f7d58aad69c6549ed9
Lisa LONCHAMP <lloncham@e2r4p14.42.fr>
2019-02-25 13:50:47 +0100

    tools.c

--- src/tools.c
+++ src/tools.c
@@ -1,20 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*   tools.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lloncham <lloncham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/25 10:43:48 by lloncham          #+#    #+#             */
-/*   Updated: 2019/02/25 13:49:00 by lloncham         ###   ########.fr       */
+/*   Created: 2019/02/25 13:49:27 by lloncham          #+#    #+#             */
+/*   Updated: 2019/02/25 13:50:11 by lloncham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WOLF3D_H
-# define WOLF3D_H
+#include "wolf3d.h"
 
-#include "../libft/libft.h"
-#include "../minilibx_macos/mlx.h"
-#include <math.h> 
+void	error(char *str)
+{
+	ft_putendl(str);
+	exit(0);
+}
 
-#endif
+int		clear_img(t_mlx *ptr)
+{
+	bzero(ptr->img_data, ptr->size_w * ptr->size_h * 4);
+	return (0);
+}
+
+void	ft_put_pixel(t_mlx *p, int y, int x, int color)
+{
+	if (x < 0 || y < 0 || x >= p->size_w || y >= p->size_h)
+		return ;
+	p->img_data[y * p->size_w + x] = color;
+}
