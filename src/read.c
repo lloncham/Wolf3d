5caf428033a770329a299ee5c5341b31bde415cc
Lisa LONCHAMP <lloncham@e1r8p6.42.fr>
2019-02-26 11:02:03 +0100

    all read ok

--- src/read.c
+++ src/read.c
@@ -6,7 +6,7 @@
 /*   By: lloncham <lloncham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 14:47:42 by lloncham          #+#    #+#             */
-/*   Updated: 2019/02/25 14:51:22 by lloncham         ###   ########.fr       */
+/*   Updated: 2019/02/26 11:00:21 by lloncham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int		count_line(int fd, char **av)
 	int		nbline;
 	int		ret;
 
+	nbline = 0;
 	i = 0;
 	if ((fd = open(av[1], O_RDONLY)) == -1)
 		return (0);
@@ -39,6 +40,28 @@ int		ft_count_col(char *split)
 	while (split[i])
 		i++;
 	return (i);
+}
+
+int			contour(t_wolf *map)
+{
+	int x;
+	int y;
+
+	x = 0;
+	y = 0;
+	while (x < map->nbc)
+	{
+		if (map->tab[0][x] != 1 || map->tab[map->nbl - 1][x] != 1)
+			return (0);
+		x++;
+	}
+	while (y < map->nbl)
+	{
+		if (map->tab[y][0] != 1 || map->tab[y][map->nbc - 1] != 1)
+			return (0);
+		y++;
+	}
+	return (1);
 }
 
 int		*convert(int **tab, int j, char *split, int nbcol)
@@ -67,7 +90,7 @@ void	read_line(int fd, t_wolf *d)
 	j = 0;
 	if (!(d->tab = (int **)malloc(sizeof(int *) * d->nbl)))
 		return ;
-	while (get_next_line(fd, &line) > 0)
+	while (get_next_line(fd, &line))
 	{
 		if (valid_char(line, d, j) == 0)
 			error("Unvalid char!");
@@ -78,6 +101,8 @@ void	read_line(int fd, t_wolf *d)
 		d->tab[j] = convert(d->tab, j, line, d->nbc);
 		j++;
 	}
+	d->j != 1 ? error("Mauvaise position.") : 0;
+	contour(d) == 0 ? error("Pas de contours.") : 0;
 }
 
 t_wolf	read_file(char **av)
